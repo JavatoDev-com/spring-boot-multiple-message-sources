4cdd08efdd0c2f92d3d3d4bb8e1171ce4f98e576
javatodev <sendtospringboot@gmail.com>
2021-02-01 15:59:46 +0530

    message sources

---
+++ src/main/java/com/javatodev/api/config/AppConfiguration.java
@@ -0,0 +1,23 @@
+package com.javatodev.api.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames(
+                "classpath:/messages/api_error_messages",
+                "classpath:/messages/api_response_messages"
+        );
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
